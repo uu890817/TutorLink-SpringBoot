bdbd2d8b204abeba4e165a015b8ff2f42fc75eef
Gong Chi <40139252@gm.nfu.edu.tw>
2023-08-23 09:20:42 +0800

    add tutorlinkApplication.java

---
+++ src/main/java/tw/tutorlink/TutorLinkApplication.java
@@ -0,0 +1,12 @@
+package tw.tutorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorLinkApplication.class, args);
+	}
+}
