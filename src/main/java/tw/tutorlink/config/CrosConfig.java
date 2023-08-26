527deb49a4513334e3c64f7cc2dc9d13ecfb9030
裕(8600K) <uu890817@gmail.com>
2023-08-26 13:41:34 +0800

    添加跨域(cros)設定檔
    1.添加config package
    2.修改.gitignore 添加對/bin/的排除

---
+++ src/main/java/tw/tutorlink/config/CrosConfig.java
@@ -0,0 +1,21 @@
+package tw.tutorlink.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrosConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")//所有API支持跨域
+					.allowedOrigins("http:/localhost:8080", "http:/localhost:8081")//允許域名
+					.allowCredentials(true)//是否發送Cookie
+					.allowedMethods("*")//允許那些請求方法
+//					.maxAge(3600)//跨域允許時間
+					;
+	}	
+
+	
+}
