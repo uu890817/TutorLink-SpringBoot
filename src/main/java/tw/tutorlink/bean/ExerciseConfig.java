a7214c217928b548501fea8721794e01fc68df62
YuGe <uu890817@gmail.com>
2023-09-06 17:00:06 +0800

    更新試卷相關功能

--- src/main/java/tw/tutorlink/bean/ExerciseConfig.java
+++ src/main/java/tw/tutorlink/bean/ExerciseConfig.java
@@ -45,4 +45,69 @@ public class ExerciseConfig {
 	@Column(name = "FinishShowAnswer")
 	private boolean finishShowAnswer;
 
+	
+	public Integer getExerConfigId() {
+		return exerConfigId;
+	}
+
+	public void setExerConfigId(Integer exerConfigId) {
+		this.exerConfigId = exerConfigId;
+	}
+
+	public Exercises getExercises() {
+		return exercises;
+	}
+
+	public void setExercises(Exercises exercises) {
+		this.exercises = exercises;
+	}
+
+	public ExercisePermissions getExercisePermissions() {
+		return exercisePermissions;
+	}
+
+	public void setExercisePermissions(ExercisePermissions exercisePermissions) {
+		this.exercisePermissions = exercisePermissions;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public Integer getTimeCountDown() {
+		return timeCountDown;
+	}
+
+	public void setTimeCountDown(Integer timeCountDown) {
+		this.timeCountDown = timeCountDown;
+	}
+
+	public boolean isFinishShowAnswer() {
+		return finishShowAnswer;
+	}
+
+	public void setFinishShowAnswer(boolean finishShowAnswer) {
+		this.finishShowAnswer = finishShowAnswer;
+	}
+
 }
--- src/main/java/tw/tutorlink/repository/ExercisesDAO.java
+++ src/main/java/tw/tutorlink/repository/ExercisesDAO.java
@@ -14,10 +14,11 @@ public interface ExercisesDAO extends JpaRepository<Exercises, Integer> {
 	@Query("FROM Exercises e JOIN e.lesson WHERE e.users.usersId = :usersId")
 	public List<Exercises> findByUsers(@Param("usersId") Integer usersId);
 	
-	@Query("FROM Lessons e WHERE e.users.usersId = :usersId")
+	@Query("FROM Lessons l WHERE l.users.usersId = :usersId")
 	public List<Lessons> findLessonsByUsers(@Param("usersId") Integer usersId);
 	
-	
+	@Query("FROM Exercises e WHERE e.exerId = :exerId")
+	public Exercises findExercisesByExerId(@Param("exerId") Integer exerId);
 	
 	
 	
