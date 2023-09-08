f31b07bed83f7cf8b061fa7863ef77da4ad3e457
exin <ray29.liao@gamil.com>
2023-09-08 10:45:59 +0800

    更新LessonDetailDAO跟Service，lessonController

--- src/main/java/tw/tutorlink/repository/LessonsDAO.java
+++ src/main/java/tw/tutorlink/repository/LessonsDAO.java
@@ -3,6 +3,8 @@ package tw.tutorlink.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tw.tutorlink.bean.Lessons;
 import tw.tutorlink.bean.Users;
@@ -12,4 +14,11 @@ public interface LessonsDAO extends JpaRepository<Lessons, Integer> {
 	List<Lessons> findByLessonNameContaining(String lessonName);
 	
 	List<Lessons> findByUsers(Users users);
+	
+	@Query("from Lessons where lessonId = :id")
+	public Lessons findById(@Param("id") int id);
+	
+	@Query("from Lessons l join l.lessondetail where lessonId = :id")
+	public Lessons findByIdDetail(@Param("id")int id);
+	
 }
