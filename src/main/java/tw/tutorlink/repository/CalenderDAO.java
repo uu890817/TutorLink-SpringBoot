1a214f8bcce889a18bcfd2e0f02fb74ceebbc9dd
WeiC <orz556683@gmail.com>
2023-09-11 11:27:21 +0800

    新增 行事曆CRUD

--- src/main/java/tw/tutorlink/repository/CalenderDAO.java
+++ src/main/java/tw/tutorlink/repository/CalenderDAO.java
@@ -2,13 +2,20 @@ package tw.tutorlink.repository;
 
 
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import tw.tutorlink.bean.Calender;
 
 public interface CalenderDAO extends JpaRepository<Calender, Integer> {
+	@Query("SELECT c FROM Users u JOIN u.calender c")
+	List<Calender> findAllReportList();
 
-	@Query("FROM Users u JOIN u.calender WHERE u.usersId = ?1")
-	Optional<Calender> findById(Integer usersId);
+	@Query("SELECT c FROM Users u JOIN u.calender c WHERE u.usersId = :usersId")
+	List<Calender> findCalenderListByUsersId(@Param("usersId") Integer usersId);
+	
+	@Query("SELECT c FROM Lessons l JOIN l.calender c WHERE l.lessonId = :lessonId")
+	List<Calender> findCalenderListByLessonId(@Param("lessonId") Integer lessonId);
 }
--- src/main/java/tw/tutorlink/service/CalenderService.java
+++ src/main/java/tw/tutorlink/service/CalenderService.java
@@ -4,23 +4,71 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
-
-
 import tw.tutorlink.bean.Calender;
+import tw.tutorlink.bean.Lessons;
+import tw.tutorlink.bean.Report;
+import tw.tutorlink.bean.Users;
 import tw.tutorlink.repository.CalenderDAO;
+import tw.tutorlink.repository.LessonsDAO;
+import tw.tutorlink.repository.UsersDAO;
 
 @Service
 public class CalenderService {
 
 	@Autowired
 	private CalenderDAO cDAO;
+
+	@Autowired
+	private LessonsDAO lDAO;
+
+	@Autowired
+	private UsersDAO uDao;
 	
-	public Optional<Calender> findById(Integer id) {
-		Optional<Calender> list = cDAO.findById(id);
-		return list;
+	// ID查詢
+	public Calender findById(Integer id) {
+		Optional<Calender> optional = cDAO.findById(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+
+	// 新增
+	public void insert(Calender cd) {
+		cDAO.save(cd);
+	}
+
+	// 使用者ID查詢
+	public List<Calender> findCalenderListByUsersId(Integer usersId) {
+		List<Calender> calenderList = cDAO.findCalenderListByUsersId(usersId);
+		return calenderList;
+	}
+
+	// 課程ID查詢
+	public List<Calender> findCalenderListByLessonId(Integer lessonId) {
+		List<Calender> calenderList = cDAO.findCalenderListByLessonId(lessonId);
+		return calenderList;
+	}
+
+	// ID刪除
+	public void deleteById(Integer id) {
+		cDAO.deleteById(id);
+	}
+
+	// 課程ID查詢課程
+	public Lessons findLessonsById(Integer id) {
+		Optional<Lessons> lessons = lDAO.findById(id);
+		if (lessons.isPresent()) {
+			return lessons.get();
+		}
+		return null;
+	}
+
+	// 使用者ID查詢使用者
+	public Users findUserId(Integer uID) {
+		return uDao.findById(uID).get();
 	}
 
 }
