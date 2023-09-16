d3657b3302811b870ec9f49230a8a1b112fa7e50
Chi <40139252@gm.nfu.edu.tw>
2023-09-16 15:13:20 +0800

    update
    更新忘記密碼驗證相關

--- src/main/java/tw/tutorlink/controller/MailVerify.java
+++ src/main/java/tw/tutorlink/controller/MailVerify.java
@@ -1,13 +1,16 @@
 package tw.tutorlink.controller;
 
-import java.util.Random;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpSession;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import tw.tutorlink.bean.Users;
 import tw.tutorlink.service.MailService;
 import tw.tutorlink.service.UsersService;
@@ -23,22 +26,49 @@ public class MailVerify {
 
 	@PostMapping("/forgetmail")
 	@ResponseBody
-	public String forgetMail(String mail) {
-		Random random = new Random();
-		int min = 100000; // 最小值（包括）
-		int max = 999999; // 最大值（包括）
-		int randomNumber = random.nextInt(max - min + 1) + min;
+	public String forgetMail(@RequestBody Object mail) {
+//		Random random = new Random();
+//		int min = 100000; // 最小值（包括）
+//		int max = 999999; // 最大值（包括）
+//		int randomNumber = random.nextInt(max - min + 1) + min;
+//		System.out.println(mail.toString());
+//		String mailcheck = mail.toString();
+//		// 檢查輸入的信箱是否存在，存在則寫入產生的隨機驗證碼進資料庫，以便識別
+//		Users result = uService.forgetMail(mailcheck, randomNumber);
+//		System.out.println("隨機生成的數值: " + randomNumber);
+//
+//		if (result != null) {
+//			// 當回傳不為空時，透過java寄信給前端回傳的mail，並寫入該產生的密碼
+//			String mailresult = mService.sendVerifyMail(mailcheck, randomNumber);
+//			if (mailresult.equals("成功寄信")) {
+//				return "ok";
+//			}
+//		}
+//		return "fail";
+		return null;
+	}
 
-		// 檢查輸入的信箱是否存在，存在則寫入產生的隨機驗證碼進資料庫，以便識別
-		Users result = uService.forgetMail(mail, randomNumber);
-		System.out.println("隨機生成的數值: " + randomNumber);
+	@PostMapping("/sendverify")
+	@ResponseBody
+	public String sendVerify(@RequestBody String data) {
+		JsonObject jsonObject = JsonParser.parseString(data).getAsJsonObject();
+		String mail = jsonObject.get("mail").getAsString();
+		int verify = jsonObject.get("verify").getAsInt();
+		System.out.println(mail);
+		System.out.println(verify);
+		Users result = uService.checkMail(mail);
+		System.out.println(result.getRamdonVerify());
+		Date nowDate = new Date();
+		Date expiredDate = result.getExpiredTime();
 
-		if (result != null) {
-			// 當回傳不為空時，透過java寄信給前端回傳的mail，並寫入該產生的密碼
-			String mailresult = mService.sendVerifyMail(mail, randomNumber);
-			if (mailresult.equals("成功寄信")) {
-				return "ok";
+		long minutes = ((nowDate.getTime()) - (expiredDate.getTime())) / (1000 * 60);
+		System.out.println("相差: " + minutes + " 分鐘");
+
+		if (result.getRamdonVerify() == verify) {
+			if(minutes <= 5) {
+				return "success";
 			}
+			return "overtime";
 		}
 		return "fail";
 	}
